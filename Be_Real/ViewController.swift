bfec63b984e0d8793e60cc07c570740258192ea2
codeBender31 <ibarraisrael949@yahoo.com>
2024-03-07 00:06:02 -0600

    Initial Commit

---
+++ Be_Real/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Be_Real
+//
+//  Created by Bender on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
