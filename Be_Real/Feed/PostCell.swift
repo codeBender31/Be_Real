f7f2df3be933833f2158a6ddee97e4d893701440
codeBender31 <ibarraisrael949@yahoo.com>
2024-03-15 19:09:44 -0500

    Part 3  Committ

--- Be_Real/Feed/PostCell.swift
+++ Be_Real/Feed/PostCell.swift
@@ -18,10 +18,15 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
+    
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
     private var imageDataRequest: DataRequest?
     
     func configure(with post: Post) {
         // TODO: Pt 1 - Configure Post Cell
+
         // Username
         if let user = post.user {
             usernameLabel.text = user.username
@@ -30,7 +35,7 @@ class PostCell: UITableViewCell {
         // Image
         if let imageFile = post.imageFile,
            let imageUrl = imageFile.url {
-            
+
             // Use AlamofireImage helper to fetch remote image from URL
             imageDataRequest = AF.request(imageUrl).responseImage { [weak self] response in
                 switch response.result {
@@ -51,16 +56,39 @@ class PostCell: UITableViewCell {
         if let date = post.createdAt {
             dateLabel.text = DateFormatter.postFormatter.string(from: date)
         }
+
+        // TODO: Pt 2 - Show/hide blur view
+        // A lot of the following returns optional values so we'll unwrap them all together in one big `if let`
+        // Get the current user.
+        if let currentUser = User.current,
+
+            // Get the date the user last shared a post (cast to Date).
+           let lastPostedDate = currentUser.lastPostedDate,
+
+            // Get the date the given post was created.
+           let postCreatedDate = post.createdAt,
+
+            // Get the difference in hours between when the given post was created and the current user last posted.
+           let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
+
+            // Hide the blur view if the given post was created within 24 hours of the current user's last post. (before or after)
+            blurView.isHidden = abs(diffHours) < 24
+        } else {
+
+            // Default to blur if we can't get or compute the date's above for some reason.
+            blurView.isHidden = false
+        }
+
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        // TODO: P1 - Cancel image download
+        // TODO: Pt 1 - Cancel image data request
+
         // Reset image view image.
         postImageView.image = nil
 
         // Cancel image request.
         imageDataRequest?.cancel()
-
     }
 }
